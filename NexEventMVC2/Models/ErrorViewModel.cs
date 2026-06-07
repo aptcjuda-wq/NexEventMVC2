596de813531d454cf21ce2db9a8270a19ae3cd60
Administrator <Administrator@MTR2-M2>
2026-06-07 16:42:27 +0300

    Add project files.

---
+++ NexEventMVC2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NexEventMVC2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
