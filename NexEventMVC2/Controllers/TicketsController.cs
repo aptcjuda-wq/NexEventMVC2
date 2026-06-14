c71da80461f49930955c25aad4559089d57bb01c
ramad <ramad@ARAJUDA>
2026-06-14 17:39:40 +0300

    Phase 15 step 26

--- NexEventMVC2/Controllers/TicketsController.cs
+++ NexEventMVC2/Controllers/TicketsController.cs
@@ -4,7 +4,17 @@ namespace NexEventMVC2.Controllers
 {
     public class TicketsController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public TicketsController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> MyTickets()
         {
             return View();
         }
